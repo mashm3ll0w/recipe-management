ae63ddd8484f9267b4f2e73e2448ff38f340d1ab
Charles Swaleh <charlswall@live.com>
2024-05-18 10:29:50 +0300

    created the recipe model and registered it in the admin panel

--- recipemgmt/models.py
+++ recipemgmt/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Recipe(models.Model):
+  """
+  Create a new Recipe item and add it to the database
+  """
+  DIFFICULTY_CHOICES = [
+    ("EASY", "BEGINNER"),
+    ("MEDIUM", "INTERMEDIATE"),
+    ("HARD", "EXPERT")
+  ]
+
+  title = models.CharField(max_length=255, null=False, db_index=True)
+  description = models.CharField(max_length=255, null=False)
+  ingredients = models.TextField(null=False)
+  cooking_instructions = models.TextField(null=False)
+  difficulty_level = models.CharField(choices=DIFFICULTY_CHOICES, max_length=50, null=False)
+
+
+  def __str__(self) -> str:
+    return self.title
